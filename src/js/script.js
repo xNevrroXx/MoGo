2be8613791cbca497f824abd8a8d95620d3118eb
Vladimir <govorov2001gv@gmail.com>
2021-10-12 01:16:53 +0300

    finished also 5 blocks. JS mini-refactoring about style hover images was done. Add new style to hover effect images

--- src/js/script.js
+++ src/js/script.js
@@ -29,24 +29,43 @@ const aboutUsImagesWithHover = new ObjCards(
         ],
         2
     ),
-    ourWorkImagesWithHover = new ObjCards(
-        ".our-work",
-        [],
+    ourWorksImagesWithHover = new ObjCards(
+        ".our-works",
+        [
+            //left side
+            `url("img/our-works_block/human_1.jpg")`,
+            `url("img/our-works_block/architecture_2.jpg")`,
+            `url("img/our-works_block/bridge_3.jpg")`,
+            `url("img/our-works_block/sun-through-the-walls_4.jpg")`,
+            //right side
+            `url("img/our-works_block/sea_1.jpg")`,
+            `url("img/our-works_block/glass-panorama_2.jpg")`,
+            `url("img/our-works_block/skyscraper_3.jpg")`
+        ],
         3
     );
 
 const wrapperImgs_enter = function (object) {
     return function() {
         let wrapperImgs = this.parentElement;
-        wrapperImgs.style.cssText = `
-            background-color: #95e1d3;
-        `;
-        const wrapperImg = this;
-        wrapperImg.style.cssText += `
-            transform: translate(-10px, -10px);
+        let wrapperImg = this;
+        if(this.parentElement.classList.contains('cards-with-hover__for-hoverBG_collage')) {
+            //if theare is no COLLAGE then need to transform
+            wrapperImg.style.cssText += `
             background-image: linear-gradient(to top, rgba(252, 227, 138, 0.9) 0%, rgba(243, 129, 129, 0.9) 100%),
                               ${object.arrUrlImages[wrapperImg.dataset.index]};
-        `;
+            `;
+        } else {
+            wrapperImgs.style.cssText = `
+                background-color: #95e1d3;
+            `;
+            wrapperImg.style.cssText += `
+                transform: translate(-10px, -10px); 
+                background-image: linear-gradient(to top, rgba(252, 227, 138, 0.9) 0%, rgba(243, 129, 129, 0.9) 100%),
+                                  ${object.arrUrlImages[wrapperImg.dataset.index]};
+            `;
+        }
+
         if (object.styleOfAnimation == 1) {
             wrapperImg.insertAdjacentHTML("beforeend", `
                 <img src="icons/about-us_block/USERS.png" alt="users picture" alt="happy users picture" class="cards-with-hover__hover-image">
@@ -97,8 +116,20 @@ const wrapperImgs_leave = function (object) {
     };
 };
 
-setImagesAtPage( [aboutUsImagesWithHover, ourTeamImagesWithHover] );
-setListenerForAllImagesWithHoverAtPage( [aboutUsImagesWithHover, ourTeamImagesWithHover] );
+setImagesAtPage( 
+    [
+        aboutUsImagesWithHover, 
+        ourTeamImagesWithHover,
+        ourWorksImagesWithHover
+    ]
+);
+setListenerForAllImagesWithHoverAtPage( 
+    [
+        aboutUsImagesWithHover, 
+        ourTeamImagesWithHover,
+        ourWorksImagesWithHover
+    ]
+);
 
 
 //bootstrap 
@@ -106,10 +137,9 @@ accordionButton.forEach( btn => {
     btn.style.cssText = "";
 });
 
-
 //tiny slider
 const slider = tns({
-  container: '.slider-for-qoutes',
+  container: '.slider-for-quotes',
   items: 1,
   slideBy: 1,
   autoplay: false,
@@ -119,14 +149,35 @@ const slider = tns({
   nav: false,
   controls: false,
 });
-
 document.querySelector('.btn-prev').addEventListener("click", function () {
     slider.goTo("prev");
+    console.log(window.getComputedStyle(document.querySelector('.slider-for-quotes')).width);
 });
 
 document.querySelector('.btn-next').addEventListener("click", function () {
     slider.goTo("next");
 });
+
+const slider2 = tns({
+    container: '.slider-for-quotes-2',
+    items: 1,
+    slideBy: 1,
+    autoplay: false,
+    center: true,
+    touch: true,
+    loop: true,
+    nav: false,
+    controls: false,
+  });
+  document.querySelector('.btn-prev-2').addEventListener("click", function () {
+    slider2.goTo("prev");
+});
+
+document.querySelector('.btn-next-2').addEventListener("click", function () {
+    slider2.goTo("next");
+});
+
+
 
 
 
